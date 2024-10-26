7da34212fe1ed451b49ec4bc50e3b0504652b335
Hardik Mandanka <hardikmandankaa@gmail.com>
2024-10-26 16:54:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -129,16 +129,86 @@ app.get('/getSupportedCoins', async (req, res) => {
 
 // Call the function to get supported coins
 // getSupportedCoins();
-const pinataSDK = require('@pinata/sdk');
+// const pinataSDK = require('@pinata/sdk');
+const { PinataSDK } = require("pinata")
+const fs = require('fs');
+//const pinata = new pinataSDK('8a653703e62ec8cb9a6a', '13a0328e7804600ee9fe84cf60e813e93add2f9efd1e81d4e3946bc3ea253c93');
+const pinata = new PinataSDK({
+    pinataJwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJiMzRjZjhkOC1kNWMxLTQxNDQtYTRkOC1lMWExODhkMWI5MjIiLCJlbWFpbCI6ImhhcmRpa21hbnNhcmFhQGdtYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI4YTY1MzcwM2U2MmVjOGNiOWE2YSIsInNjb3BlZEtleVNlY3JldCI6IjEzYTAzMjhlNzgwNDYwMGVlOWZlODRjZjYwZTgxM2U5M2FkZDJmOWVmZDFlODFkNGUzOTQ2YmMzZWEyNTNjOTMiLCJleHAiOjE3NjE0NTk4MTd9.80SF24JrnW4AGQblJ8OOStqq-KRxU1x66i53PJph-Ow",
+    pinataGateway: "moccasin-many-turtle-335.mypinata.cloud"
+})
+
 
 app.post('/storeJSON', async (req, res) => {
     const jsonData = req.body;
-    const pinata = new pinataSDK('8a653703e62ec8cb9a6a', '13a0328e7804600ee9fe84cf60e813e93add2f9efd1e81d4e3946bc3ea253c93');
-
     try {
-        const result = await pinata.pinJSONToIPFS(jsonData);
-        res.send({ daat: 'https://moccasin-many-turtle-335.mypinata.cloud/ipfs/' + result.IpfsHash });
+        // const result = await pinata.pinJSONToIPFS(jsonData);
+        //generat pdf
+        await generatePDF(jsonData);
+        const blob = new Blob([fs.readFileSync("./output.pdf")]);
+        const file = new File([blob], "output.pdf", { type: "text/plain" })
+        const upload = await pinata.upload.file(file);
+        console.log(upload);
+        // res.send(upload);
+        res.send({ data: 'http://localhost:3000/readFile?hash=' + upload.cid });
+        // res.send({ data: 'https://moccasin-many-turtle-335.mypinata.cloud/ipfs/' + result.IpfsHash });
     } catch (error) {
         console.error('Error setting up Helia:', error)
     }
 })
+
+
+app.get('/readFile', async (req, res) => {
+    const _hash = req.query.hash;
+    try {
+        const file = await pinata.gateways.get(_hash)
+        const arrayBuffer = await file.data.arrayBuffer();
+        const pdfBuffer = Buffer.from(arrayBuffer);
+        // Set response headers to trigger download
+        res.setHeader('Content-Type', 'application/pdf');
+        res.setHeader('Content-Disposition', 'attachment; filename="downloaded_file.pdf"');
+        // Send the PDF buffer as the response
+        res.send(pdfBuffer);
+    } catch (error) {
+        console.error('Error setting up Helia:', error)
+        res.send('Error setting up Helia:', error);
+    }
+})
+const PDFDocument = require('pdfkit');
+
+
+function generatePDF(data, fileName = 'output.pdf') {
+    return new Promise((resolve, reject) => {
+        const doc = new PDFDocument();
+        const stream = fs.createWriteStream(fileName);
+
+        doc.pipe(stream);
+
+        function addData(data, indent = '') {
+            if (typeof data === 'object' && data !== null) {
+                if (Array.isArray(data)) {
+                    data.forEach((item, i) => {
+                        doc.text(`${indent}- [${i + 1}]`);
+                        addData(item, indent + '  ');
+                    });
+                } else {
+                    Object.entries(data).forEach(([key, value]) => {
+                        doc.text(`${indent}${key}:`);
+                        addData(value, indent + '  ');
+                    });
+                }
+            } else {
+                doc.text(`${indent}${data}`);
+            }
+        }
+
+        addData(data);
+        doc.end();
+        stream.on('finish', () => {
+            resolve();
+        });
+        stream.on('error', (err) => {
+            reject(err);
+        });
+    });
+}
